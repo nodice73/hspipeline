2edf4305d26bd70b792456b76d4ee788e86f9864
Adam Waite <adamw@calico.com>
2017-09-28 22:41:49 -0700

    updated for use on just one server.

--- web/paths.py
+++ web/paths.py
@@ -3,23 +3,9 @@ from getpass import getuser
 
 class Paths(object):
     base = '/var/www/hspipeline/web'
-    local_data = '/media/Data/hspipeline_test'
-    local_r    = '/home/ajwaite/Documents/Code/hspipeline'
-
-    shouweb_data = '/home/shouweb/shougroup'
-
-    silo_base = '/shared/silo_researcher/Shou_W'
-    silo_data = os.path.join(silo_base, 'shougroup')
-    silo_r = os.path.join(silo_base, 'code/hspipeline')
-
-    if getuser() == 'ajwaite':
-        data = local_data
-        data_web = local_data
-        r = local_r
-    else:
-        data = silo_data
-        data_web = shouweb_data
-        r = silo_r
+    data = '/home/adamw/seq_data'
+    output = os.path.join(data, 'output')
+    r = '/var/www/hspipeline'
 
     def __init__(self, proj=None, anc=None, ref=None):
         self.outlog = ''
@@ -27,4 +13,3 @@ class Paths(object):
 
         (self.proj, self.anc, self.ref) = [os.path.join(Paths.data, p)
                                            for p in [proj, anc, ref]]
-        self.proj_web = os.path.join(Paths.data_web, proj)
