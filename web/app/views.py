9a46f7fdf580de86ba6276de71d2135ced1f286a
Adam Waite <adamw@calico.com>
2017-09-29 15:48:21 -0700

    fixed outlog sent to 'finished' template.

--- web/app/views.py
+++ web/app/views.py
@@ -43,7 +43,7 @@ def running():
                                                            e.strerror))
 
     if finished:
-        return render_template('finished.html', outlog=outlog_final,
+        return render_template('finished.html', outlog=outlog,
                                outlog_final_location=outlog_final_location)
     else:
         return render_template('running.html', outlog=outlog,
